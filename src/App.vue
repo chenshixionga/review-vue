fc3560268f8497d79e2118016421f024d56ae752
chenshixiong <1067816765@qq.com>
2018-06-22 22:03:09 +0800

    xinxi

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div id="app">
+    
     <img src="./assets/logo.png">
+    <el-select v-model="value" placeholder="请选择">
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value">
+      </el-option>
+    </el-select>
     <p>导航
       <router-link to="/">首页</router-link>
       <router-link to="/hi">欢迎页</router-link>
@@ -75,7 +84,24 @@
       return {
         msg: '1234',
         messages: [],
-        vmodelTest:''
+        vmodelTest:'',
+        options: [{
+          value: '选项1',
+          label: '黄金糕'
+        }, {
+          value: '选项2',
+          label: '双皮奶'
+        }, {
+          value: '选项3',
+          label: '蚵仔煎'
+        }, {
+          value: '选项4',
+          label: '龙须面'
+        }, {
+          value: '选项5',
+          label: '北京烤鸭'
+        }],
+        value: ''
       }
     },
     components: {
